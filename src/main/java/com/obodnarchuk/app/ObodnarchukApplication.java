d8ba93f597d9022a8fbf2685260f665ec6960e41
OleksandrBodnarchuk <obodnarchuk@outlook.com>
2021-12-22 11:30:59 +0100

    Maven dependencies + configuration for DB access

--- src/main/java/com/obodnarchuk/app/ObodnarchukApplication.java
+++ src/main/java/com/obodnarchuk/app/ObodnarchukApplication.java
@@ -1,4 +1,4 @@
-package com.obodnarchuk.obodnarchuk;
+package com.obodnarchuk.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/obodnarchuk/ObodnarchukApplicationTests.java
+++ src/test/java/com/obodnarchuk/ObodnarchukApplicationTests.java
@@ -1,4 +1,4 @@
-package com.obodnarchuk.obodnarchuk;
+package com.obodnarchuk;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
