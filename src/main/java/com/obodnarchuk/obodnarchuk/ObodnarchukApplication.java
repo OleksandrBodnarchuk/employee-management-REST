0a28b0fce5712b9f1c67650e2df0edee5a93c610
OleksandrBodnarchuk <obodnarchuk@outlook.com>
2021-12-22 09:54:29 +0100

    Initial commit

---
+++ src/main/java/com/obodnarchuk/obodnarchuk/ObodnarchukApplication.java
@@ -0,0 +1,13 @@
+package com.obodnarchuk.obodnarchuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObodnarchukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObodnarchukApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/obodnarchuk/obodnarchuk/ObodnarchukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obodnarchuk.obodnarchuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObodnarchukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
