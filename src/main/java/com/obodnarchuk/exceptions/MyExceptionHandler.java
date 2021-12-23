4880dad39a76d9e55b65285e72ff2015e69b129c
OleksandrBodnarchuk <obodnarchuk@outlook.com>
2021-12-23 15:52:18 +0100

    Exceptions for not found records

---
+++ src/main/java/com/obodnarchuk/exceptions/MyExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.obodnarchuk.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class MyExceptionHandler extends RuntimeException{
+
+    private final String RECORD_NOT_FOUND ="RECORD_NOT_FOUND";
+    private final String INCORRECT_REQUEST="INCORRECT_REQUEST";;
+
+    @ExceptionHandler(RecordNotFoundException.class)
+    public final ResponseEntity<ErrorResponse> handleUserNotFoundException
+            (RecordNotFoundException ex, WebRequest request)
+    {
+        List<String> details = new ArrayList<>();
+        details.add(ex.getLocalizedMessage());
+        ErrorResponse error = new ErrorResponse(RECORD_NOT_FOUND, details);
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+}
